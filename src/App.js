6b57d8c39b01120749e394ab1bb90a4b41fc42e2
mmasson <maximemasson30@gmail.com>
2022-01-30 21:26:23 -0500

    awwards rebuilt part 3

--- src/App.js
+++ src/App.js
@@ -1,19 +1,59 @@
 import Banner from "./components/banner";
 import Header from "./components/header";
+import gsap from "gsap";
 import "./styles/App.scss";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import Cases from "./components/cases";
+import IntroOverlay from "./components/introOverlay";
 
 function App() {
+    const title = useRef(null);
+
     useEffect(() => {
         let vh = window.innerHeight * 0.01;
         document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+        // prevent flashing
+        gsap.to("body", 0, { css: { visibility: "visible" } });
+
+        // timeline
+        const tl = gsap.timeline();
+        // tl.from(title.current, { duration: 2, opacity: 0, y: 70 });
+        tl.from(".line span", {
+            duration: 1.8,
+            y: 100,
+            ease: "power4.out",
+            delay: 1,
+            skewY: 7,
+            stagger: {
+                amount: 0.3,
+            },
+        })
+            .to(".overlay-top", { duration: 1.6, height: 0, ease: "expo.inOut", stagger: 0.4 })
+            .to(".overlay-bottom", {
+                duration: 1.6,
+                width: 0,
+                ease: "expo.inOut",
+                delay: -0.8,
+                stagger: {
+                    amount: 0.4,
+                },
+            })
+            .to(".intro-overlay", { duration: 0, css: { display: "none" } })
+            .from(".case-image img", {
+                duration: 1.6,
+                scale: 1.4,
+                ease: "expo.inOut",
+                delay: -2,
+                stagger: { amount: 0.4 },
+            });
     }, []);
     return (
         <div className="App">
+            <IntroOverlay />
             <Header />
-            <Banner />
-            <Cases/>
+            <Banner title={title} />
+            <Cases />
         </div>
     );
 }
--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { ReactComponent as RightArrow } from "../assets/arrow-right.svg"
+import { ReactComponent as RightArrow } from "../assets/arrow-right.svg";
 
-export default function Banner() {
+export default function Banner({ title }) {
     return (
         <section className="main">
             <div className="container">
                 <div className="row">
-                    <h2>
+                    <h2 ref={title}>
                         <div className="line">
                             <span>Creating unique brands is</span>
                         </div>
@@ -15,7 +15,9 @@ export default function Banner() {
                         </div>
                     </h2>
                     <div className="btn-row">
-                        <a href="/">More about us <RightArrow/></a>
+                        <a href="/">
+                            More about us <RightArrow />
+                        </a>
                     </div>
                 </div>
             </div>
